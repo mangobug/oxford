fb02ab92b27527ec450c4a236b1f840630e623ea
Azan Abrar <azan.abrar@gmail.com>
2018-04-23 18:43:25 +0500

    Worked on feedback points and designed contributors page static template

---
+++ wp-content/themes/simplegridtheme/tpl-contributors.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+
+  Template Name: Contributors
+
+*/
+
+?>
+
+
+
+<?php get_header(); ?>
+
+    <div id="Contributors">
+        <div class="head-block">
+            <div class="container">
+                <h2>Authors Page</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+        </div>
+        <div class="Contributors-info">
+            <div class="container">
+                <div class="img-box"><img src="http://1.gravatar.com/avatar/4e4af3127cb44bc9ecba4fc1fe2ef825?s=114&d=mm&r=g" alt="image description"></div>
+                <div class="txt-holder">
+                    <span class="name">Michael Turner</span>
+                    <span class="description">Job Title</span>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <div class="footer-area">
+                        <a class="btn-more" href="#">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="Contributors-info">
+            <div class="container">
+                <div class="img-box"><img src="http://1.gravatar.com/avatar/4e4af3127cb44bc9ecba4fc1fe2ef825?s=114&d=mm&r=g" alt="image description"></div>
+                <div class="txt-holder">
+                    <span class="name">Ann Clavella</span>
+                    <span class="description">Job Title</span>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <div class="footer-area">
+                        <a class="btn-more" href="#">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+<?php get_footer(); ?>
