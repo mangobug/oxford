febcd4eebc84183c4dae60f73e745e11803edff9
Zain Khan <Zain.Khan@hkstrategies.ca>
2018-07-02 19:07:46 -0400

    Grayscale images for IE

--- wp-content/themes/simplegridtheme/index.php
+++ wp-content/themes/simplegridtheme/index.php
@@ -242,6 +242,45 @@ $('.load_more_cont a').live('click', function(e) {
 
 </script>
 
+<script>
+
+function gray(imgObj) {
+    var canvas = document.createElement('canvas');
+    var canvasContext = canvas.getContext('2d');
+
+    var imgW = 300;
+    var imgH = 250;
+    canvas.width = imgW;
+    canvas.height = imgH;
+
+    canvasContext.drawImage(imgObj, 0, 0);
+    var imgPixels = canvasContext.getImageData(0, 0, imgW, imgH);
+
+    for(var y = 0; y < imgPixels.height; y++){
+        for(var x = 0; x < imgPixels.width; x++){
+            var i = (y * 4) * imgPixels.width + x * 4;
+            var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+            imgPixels.data[i] = avg;
+            imgPixels.data[i + 1] = avg;
+            imgPixels.data[i + 2] = avg;
+        }
+    }
+    canvasContext.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+    return canvas.toDataURL();
+}
+
+var ua = window.navigator.userAgent;
+var msie = ua.indexOf("MSIE ");
+
+if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+  console.log("IE");
+  var imgObj = document.getElementsByClassName('wp-post-image');
+  for (var i = 0; i < imgObj.length; i++) {
+    imgObj[i].src = gray(imgObj[i]);
+  }
+} else { console.log("IE not found"); }
+
+</script>
 
 <?php get_footer(); ?>
 
